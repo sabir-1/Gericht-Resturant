5dda173ba3c27f89c6afec128e103e6c2b360526
Noman Khokher <nomankhokher786@gmail.com>
2022-03-25 00:00:08 +0500

    first commit

---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './FooterOverlay.css';
+
+const FooterOverlay = () => {
+  
+return(
+  <div>
+    FooterOverlay
+  </div>
+)
+}
+
+export default FooterOverlay;
