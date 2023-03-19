c64824a2e3c80bcc5752b3f4a7bc0529a09c2b2d
sabir-1 <toplinks56@gmail.com>
2023-03-20 00:14:16 +0500

    update  style in slider file

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({title, price, tags}) => (
       <div className='app__menuitem-name'>
         <p className='p__cormorant' style={{color: '#DCCA87'}}>{title}</p>
       </div>
-    </div> 
+    </div>  
   </div>
 );
 
