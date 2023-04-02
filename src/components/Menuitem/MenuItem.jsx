8a87d721446cfe91169451686e649080e9a97ccc
sabir-1 <toplinks56@gmail.com>
2023-04-02 05:25:14 +0500

    update id

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './MenuItem.css';
 
 const MenuItem = ({title, price, tags}) => (  
-  <div className='app__menuitem'>
+  <div className='app__menuitem'> 
     <div className='app__menuitem-head'> 
       <div className='app__menuitem-name'>
         <p className='p__cormorant tailwind-props' style={{color: '#DCCA87'}}>{title}</p> 
