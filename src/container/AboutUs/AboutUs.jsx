7bce6851386c6dd051069da9aec92461117f7fa4
sabir-1 <toplinks56@gmail.com>
2023-04-08 23:50:00 +0500

    chore(fixed issue update model)

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
 return(
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     <div className='app__aboutus-overlay flex__center'>
-      <img src={images.G} alt='g letter'/>
+      <img src={images.G} alt='g letter'/> 
     </div>
     <div className='app__aboutus-content flex__center'>
       <div className='app__aboutus-content_about'>
