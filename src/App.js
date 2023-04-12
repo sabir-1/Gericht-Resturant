128abc61272abea7c20f41b32999a694c603842e
sabir-1 <toplinks56@gmail.com>
2023-04-13 04:24:46 +0500

    --conflict fixed--

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
+import { Navbar } from './components'; 
 import './App.css';
 
 const App = () => ( 
