7dccf3824563b190c37aac7518201344e77b8ec1
sabir-1 <toplinks56@gmail.com>
2023-04-19 04:26:25 +0500

    --ui update design--

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -8,9 +8,9 @@ import './SpecialMenu.css';
 const SpecialMenu = () => {  
   return(
   <div className='app__specialMenu flex__center section__padding'> 
-    <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits you palatte"/> 
-      <h1 className='headtext__cormorant'>Today’s Special</h1> 
+    <div className="app__specialMenu-title"> 
+      <SubHeading title="Menu that fits you palatte"/>  
+      <h1 className='headtext__cormorant'>Today’s Special</h1>  
 
     </div>
     <div className='app__specialMenu-menu'>
