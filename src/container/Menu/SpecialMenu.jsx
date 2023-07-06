6df0bd3f53346a117f1374756488a27a155c6b5b
sabir-1 <toplinks56@gmail.com>
2023-07-06 14:02:52 +0500

    ---fixed--

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -15,7 +15,7 @@ const SpecialMenu = () => {
   
     <div className="app__specialMenu-title"> 
       <SubHeading title="Menu that fits you palatte"/>   
-      <h1 className='headtext__cormorant'>Today’s Special</h1>    
+      <h1 className='headtext__cormorant'>Today’s Special</h1>      
 
     </div> 
     <div className='app__specialMenu-menu'>
