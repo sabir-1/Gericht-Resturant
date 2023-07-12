9e928c92610182f863ec9eb8830149219a8f0450
sabir-1 <toplinks56@gmail.com>
2023-07-12 12:54:32 +0500

    chore(component/SpecialMenu add use state hook for hide div)

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { SubHeading, MenuItem } from '../../components';
 
 import { images, data } from '../../constants';
 import './SpecialMenu.css'; 
  
-const SpecialMenu = () => {             
+const SpecialMenu = () => {     
+  const [name, setName] = useState(null)        
   return( 
   <div className='app__specialMenu flex__center section__padding'>   
 
