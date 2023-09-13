fe90075f5df7739752b6398bd969503ecb3836c2
sabir-1 <toplinks56@gmail.com>
2023-09-13 12:46:05 +0500

    ui fixed issue

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 
-
+  
 
 const FindUs = () => (
   <div className='app__bg app__wrapper section__padding'> 
