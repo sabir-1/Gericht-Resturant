617c5bb152e2ec3d852adbef6b788b0cc61698d9
sabir-1 <toplinks56@gmail.com>
2023-09-21 14:43:39 +0500

    add function

--- src/container/index.js
+++ src/container/index.js
@@ -4,9 +4,9 @@ import FindUs from './Findus/FindUs';
 import Footer from './Footer/Footer';
 import Gallery from './Gallery/Gallery';      
 import Header from './Header/Header';
-import Intro from './Intro/Intro'; 
-import Laurels from './Laurels/Laurels';  
-import SpecialMenu from './Menu/SpecialMenu';      
+import Intro from './Intro/Intro';   
+import Laurels from './Laurels/Laurels';   
+import SpecialMenu from './Menu/SpecialMenu';       
 
 export { 
   AboutUs, 
